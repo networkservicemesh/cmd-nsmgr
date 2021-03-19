e832a71ae971824d26a42c34f4791374228e7588
Vladimir Popov <vladimir.popov@xored.com>
2021-03-19 16:04:08 +0700

    Manually update to the latest sdk version (#208)
    Signed-off-by: Vladimir Popov <vladimir.popov@xored.com>

--- internal/imports/gen.go
+++ internal/imports/gen.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2020 Cisco and/or its affiliates.
+// Copyright (c) 2020-2021 Cisco and/or its affiliates.
 //
 // SPDX-License-Identifier: Apache-2.0
 //
@@ -14,7 +14,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package imports is used for generating list of imports to optimize use of docker build cache
 package imports
 
-//go:generate bash -c "cd $(mktemp -d) && GO111MODULE=on go get github.com/edwarnicke/imports-gen@v1.0.1"
-//go:generate ${GOPATH}/bin/imports-gen
+//go:generate bash -c "rm -rf imports*.go"
+//go:generate bash -c "cd $(mktemp -d) && GO111MODULE=on go get github.com/edwarnicke/imports-gen@v1.1.0"
+//go:generate bash -c "GOOS=linux ${GOPATH}/bin/imports-gen"
