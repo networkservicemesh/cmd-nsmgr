71a1805cc8b08eadb3c22844f39c3e6fd9f3000d
Vladislav Byrgazov <vladislav.byrgazov@xored.com>
2024-08-07 14:03:03 +0500

    Add additional profiling http handlers
    Signed-off-by: Vladislav Byrgazov <vladislav.byrgazov@xored.com>

--- main.go
+++ main.go
@@ -100,6 +100,12 @@ func main() {
 		mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
 		mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
 		mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
+		mux.Handle("/debug/pprof/allocs", pprof.Handler("allocs"))
+		mux.Handle("/debug/pprof/block", pprof.Handler("block"))
+		mux.Handle("/debug/pprof/goroutine", pprof.Handler("goroutine"))
+		mux.Handle("/debug/pprof/heap", pprof.Handler("heap"))
+		mux.Handle("/debug/pprof/mutex", pprof.Handler("mutex"))
+		mux.Handle("/debug/pprof/threadcreate", pprof.Handler("threadcreate"))
 		server := &http.Server{
 			Addr:         "localhost:" + cfg.PprofPort,
 			Handler:      mux,
