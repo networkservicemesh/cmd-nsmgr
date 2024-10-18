886a8421ffc531722410fa12c837781ec1310e6c
Vladislav Byrgazov <vladislav.byrgazov@xored.com>
2024-10-18 14:05:51 +0500

    Update go to 1.23
    Signed-off-by: Vladislav Byrgazov <vladislav.byrgazov@xored.com>

--- internal/manager/manager.go
+++ internal/manager/manager.go
@@ -2,7 +2,7 @@
 //
 // Copyright (c) 2022 Nordix and/or its affiliates.
 //
-// Copyright (c) 2023 Cisco and/or its affiliates.
+// Copyright (c) 2023-2024 Cisco and/or its affiliates.
 //
 // SPDX-License-Identifier: Apache-2.0
 //
